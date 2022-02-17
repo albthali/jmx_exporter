f0637ad15fb1092d63baff9d6f45ffe10e0ccd6d
Abdalrahman Albthali <33764153+albthali@users.noreply.github.com>
2022-02-17 15:42:53 +0300

    Update JavaAgent.java
    Adding delay to Java Agent

--- jmx_prometheus_javaagent_java6/src/main/java/io/prometheus/jmx/JavaAgent.java
+++ jmx_prometheus_javaagent_java6/src/main/java/io/prometheus/jmx/JavaAgent.java
@@ -1,39 +1,94 @@
 package io.prometheus.jmx;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.instrument.Instrumentation;
 import java.net.InetSocketAddress;
+import java.util.Map;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.exporter.HTTPServer;
 import io.prometheus.client.hotspot.DefaultExports;
+import org.yaml.snakeyaml.Yaml;
+
+import javax.management.MalformedObjectNameException;
 
 public class JavaAgent {
 
     static HTTPServer server;
 
-    public static void agentmain(String agentArgument, Instrumentation instrumentation) throws Exception {
-        premain(agentArgument, instrumentation);
-    }
-
-    public static void premain(String agentArgument, Instrumentation instrumentation) throws Exception {
-        // Bind to all interfaces by default (this includes IPv6).
+    public static void agentmain(final String agentArgument, final Instrumentation instrumentation) throws Exception {
         String host = "0.0.0.0";
-
         try {
             Config config = parseConfig(agentArgument, host);
+            Map yaml =  new Yaml().load(new FileInputStream(config.file));
+            if (yaml.containsKey("agentDelay")){
+                final int delay = (Integer) yaml.get("agentDelay");
+                final ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
+                System.out.println("Scheduling task to run in " + delay +" seconds");
+                executor.schedule(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            System.err.println("Starting Premain After " + delay + "minute");
+                            premain(agentArgument, instrumentation);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, delay, TimeUnit.SECONDS);
+            }
+            else premain(agentArgument, instrumentation);
 
-            new BuildInfoCollector().register();
-            new JmxCollector(new File(config.file), JmxCollector.Mode.AGENT).register();
-            DefaultExports.initialize();
-            server = new HTTPServer(config.socket, CollectorRegistry.defaultRegistry, true);
         }
         catch (IllegalArgumentException e) {
             System.err.println("Usage: -javaagent:/path/to/JavaAgent.jar=[host:]<port>:<yaml configuration file> " + e.getMessage());
             System.exit(1);
         }
+
+
+    }
+
+    public static void premain(final String agentArgument, final Instrumentation instrumentation) throws Exception {
+        // Bind to all interfaces by default (this includes IPv6).
+        String host = "0.0.0.0";
+        try {
+            final Config config = parseConfig(agentArgument, host);
+            Map yaml =  new Yaml().load(new FileInputStream(config.file));
+            if (yaml.containsKey("agentDelay")){
+                final int delay = (Integer) yaml.get("agentDelay");
+                final ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
+                System.out.println("Scheduling task to run in " + delay +" seconds");
+                executor.schedule(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            System.out.println("Starting Java Prometheus Exporter Agent after " + delay + "seconds");
+                            startAgent(config);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, delay, TimeUnit.SECONDS);
+            }
+            else  startAgent(config);
+        }
+        catch (IllegalArgumentException e) {
+            System.err.println("Usage: -javaagent:/path/to/JavaAgent.jar=[host:]<port>:<yaml configuration file> " + e.getMessage());
+            System.exit(1);
+        }
+    }
+    public static void startAgent(final Config config) throws Exception {
+        new BuildInfoCollector().register();
+        new JmxCollector(new File(config.file), JmxCollector.Mode.AGENT).register();
+        DefaultExports.initialize();
+        server = new HTTPServer(config.socket, CollectorRegistry.defaultRegistry, true);
     }
 
     /**
