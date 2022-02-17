b9b13eea4e028612ff9c39f84be2f3a7decca5e6
Abdalrahman Albthali <33764153+albthali@users.noreply.github.com>
2022-02-17 16:09:00 +0300

    Update JavaAgent.java
    Using Environment Variable instead of snake yaml. Could not get Snake yaml to work

--- jmx_prometheus_javaagent_java6/src/main/java/io/prometheus/jmx/JavaAgent.java
+++ jmx_prometheus_javaagent_java6/src/main/java/io/prometheus/jmx/JavaAgent.java
@@ -1,58 +1,22 @@
 package io.prometheus.jmx;
-
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.lang.instrument.Instrumentation;
 import java.net.InetSocketAddress;
-import java.util.Map;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.exporter.HTTPServer;
 import io.prometheus.client.hotspot.DefaultExports;
-import org.yaml.snakeyaml.Yaml;
 
-import javax.management.MalformedObjectNameException;
 
 public class JavaAgent {
 
     static HTTPServer server;
 
     public static void agentmain(final String agentArgument, final Instrumentation instrumentation) throws Exception {
-        String host = "0.0.0.0";
-        try {
-            Config config = parseConfig(agentArgument, host);
-            Map yaml =  new Yaml().load(new FileInputStream(config.file));
-            if (yaml.containsKey("agentDelay")){
-                final int delay = (Integer) yaml.get("agentDelay");
-                final ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
-                System.out.println("Scheduling task to run in " + delay +" seconds");
-                executor.schedule(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            System.err.println("Starting Premain After " + delay + "minute");
-                            premain(agentArgument, instrumentation);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }, delay, TimeUnit.SECONDS);
-            }
-            else premain(agentArgument, instrumentation);
-
-        }
-        catch (IllegalArgumentException e) {
-            System.err.println("Usage: -javaagent:/path/to/JavaAgent.jar=[host:]<port>:<yaml configuration file> " + e.getMessage());
-            System.exit(1);
-        }
-
-
+        premain(agentArgument, instrumentation);
     }
 
     public static void premain(final String agentArgument, final Instrumentation instrumentation) throws Exception {
@@ -60,9 +24,10 @@ public class JavaAgent {
         String host = "0.0.0.0";
         try {
             final Config config = parseConfig(agentArgument, host);
-            Map yaml =  new Yaml().load(new FileInputStream(config.file));
-            if (yaml.containsKey("agentDelay")){
-                final int delay = (Integer) yaml.get("agentDelay");
+            final String prometheusExporterDelay = System.getenv("PROMETHEUS_EXPORTER_DELAY");
+
+            if (prometheusExporterDelay != null){
+                final int delay = Integer.parseInt(prometheusExporterDelay);
                 final ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
                 System.out.println("Scheduling task to run in " + delay +" seconds");
                 executor.schedule(new Runnable() {
